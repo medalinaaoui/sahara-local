89296e24e3237db33fc85479ae9b2eac6cb82573
Med Ali Naaoui <chayga00@gmail.com>
2023-10-14 05:55:28 +0100

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.DBURI);
+    console.log("connected successfully");
+  } catch (error) {
+    if (error) console.log("error from connecting to mongoDb: ", error);
+  }
+};
+connect();
 const PORT = 8000;
 app.listen(process.env.PORT || PORT, (err) => {
   if (!err) console.log("server runnig on port: ", PORT);
