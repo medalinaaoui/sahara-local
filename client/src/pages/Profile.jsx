5f693de5fc4293c320b5c8d0084d8cc75f2a93eb
Med Ali Naaoui <chayga00@gmail.com>
2023-10-17 03:09:11 +0100

    updates in profile componant

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,32 +1,122 @@
 import { useSelector } from "react-redux";
-import { useState } from "react";
-
+import { useState, useRef, useEffect } from "react";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import app from "../firebase";
 const Profile = () => {
   const currUser = useSelector((state) => state.user.currUser);
   const [loading, setLoading] = useState(false);
+  const [file, setfile] = useState(undefined);
+  const [uploadPer, setUploadPer] = useState(0);
+  const [formData, setformData] = useState({});
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const fileInputRef = useRef(null);
+
+  const handleInfosChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setformData({ ...formData, [name]: value });
+  };
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadPer(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+        console.log("error from uploadTask.on: ", error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setformData({ ...formData, profile_pic: downloadURL });
+          setfile(null);
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (file) handleFileUpload(file);
+  }, [file]);
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    if (!formData.username || !formData.email)
+      console.log("infos are required");
+  };
+
   return (
     <div className="w-full flex flex-col gap-3 items-center justify-center h-[90vh]">
       <h1 className="sm:text-3xl text-2xl font-semibold">Profile</h1>
-      <div className="w-28 aspect-square ">
+      <input
+        className="hidden"
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={(e) => setfile(e.target.files[0])}
+      />
+      <div className="w-28 aspect-square relative">
         <img
-          src={currUser.profile_pic}
+          src={formData.profile_pic || currUser.profile_pic}
           alt="profile pic"
           className="object-contain rounded-full"
         />
+        <FaCloudUploadAlt
+          onClick={() => fileInputRef.current.click()}
+          className="text-3xl font-bold absolute bottom-0 right-2 cursor-pointer"
+        />
+        <span
+          className={
+            uploadPer > 0 && uploadPer < 100
+              ? "absolute top-[46%] left-[37%] font-bold animate-bounce"
+              : "hidden"
+          }
+        >
+          {uploadPer}%
+        </span>
       </div>
-      <form className="flex flex-col gap-2 w-full items-center">
+      {fileUploadError ? (
+        <p className="text-red-600">
+          uploading error: image must be less than 2mb
+        </p>
+      ) : uploadPer === 100 ? (
+        <p className="text-green-600">image uploaded successfully</p>
+      ) : (
+        ""
+      )}
+
+      <form
+        onSubmit={handleUpdate}
+        className="flex flex-col gap-2 w-full items-center"
+      >
         <input
           className="sm:py-2 py-1 px-1 w-4/5 sm:w-1/2 lg:w-1/3"
-          value={currUser.username}
+          onChange={handleInfosChange}
+          defaultValue={currUser.username}
           name="username"
           type="text"
           placeholder="username"
         />
         <input
           className="sm:py-2 py-1 px-1 w-4/5 sm:w-1/2 lg:w-1/3"
-          value={currUser.email}
-          name="username"
-          type="text"
+          defaultValue={currUser.email}
+          onChange={handleInfosChange}
+          name="email"
+          type="email"
           placeholder="username"
         />
         <button
@@ -41,10 +131,16 @@ const Profile = () => {
           )}
         </button>
         <div className="flex justify-between w-4/5 sm:w-1/2 lg:w-1/3">
-          <span className="text-red-600 font-semibold cursor-pointer">
+          <span
+            type="button"
+            className="text-red-600 font-semibold cursor-pointer"
+          >
             delete account
           </span>
-          <span className="text-blue-600 font-semibold cursor-pointer">
+          <span
+            type="button"
+            className="text-blue-600 font-semibold cursor-pointer"
+          >
             Logout
           </span>
         </div>
