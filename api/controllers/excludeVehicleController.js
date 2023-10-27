19e514b6c30a466df9c7b0831cb0617100bc2f7f
Med Ali Naaoui <chayga00@gmail.com>
2023-10-27 01:37:28 +0100

    adding exclude vehicle

---
+++ api/controllers/excludeVehicleController.js
@@ -0,0 +1,29 @@
+import Vehicle from "../model/vehicle.js";
+const vehiclesListController = async (req, res, next) => {
+  const vehicleId = req.params.id;
+  const owner = req.user;
+
+  try {
+    const vehicle = await Vehicle.findOne({ _id: vehicleId });
+
+    if (!vehicle) {
+      res.status(404).json({
+        message: "Vehicle not found",
+      });
+    } else if (vehicle.owner !== owner) {
+      res.status(401).json({
+        message: "You are not the owner of this vehicle",
+      });
+    } else {
+      await Vehicle.findByIdAndDelete(vehicleId);
+      res.status(200).json({
+        status: "success",
+        message: "Vehicle has been excluded successfully",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default vehiclesListController;
--- api/routes/vehicles/vehiclesRouter.js
+++ api/routes/vehicles/vehiclesRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import includeVehicleController from "../../controllers/includeVehicleController.js";
+import excludeVehicleController from "../../controllers/excludeVehicleController.js";
 import vehiclesListController from "../../controllers/vehiclesListController.js";
 import verifyAccess from "../../utils/verifyAccess.js";
 const vehiclesRouter = express.Router();
 
 vehiclesRouter
   .post("/include", verifyAccess, includeVehicleController)
-  .get("/list/:id", verifyAccess, vehiclesListController);
+  .get("/list/:id", verifyAccess, vehiclesListController)
+  .delete("/excludeVehicle/:id", verifyAccess, excludeVehicleController);
 
 export default vehiclesRouter;
