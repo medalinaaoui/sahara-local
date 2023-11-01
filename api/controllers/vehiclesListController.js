b330df83095e9312cfbf85b5c34c9dba9bd3ad97
Med Ali Naaoui <chayga00@gmail.com>
2023-11-01 21:02:12 +0100

    adding redis to the server

--- api/controllers/vehiclesListController.js
+++ api/controllers/vehiclesListController.js
@@ -1,5 +1,14 @@
 import Vehicle from "../model/vehicle.js";
 import errorHandler from "../utils/customError.js";
+import Redis from "ioredis";
+// {
+//   host: process.env.REDIS_HOST || "redis",
+//   port: 6379,
+// }
+const redisClient = new Redis({
+  host: process.env.REDIS_HOST || "redis",
+});
+const expirationTime = 60 * 60 * 30;
 
 const vehiclesListController = async (req, res, next) => {
   const owner = req.params.id;
@@ -7,8 +16,20 @@ const vehiclesListController = async (req, res, next) => {
     next(errorHandler(402, "unauthorized"));
   } else {
     try {
-      const vehicles = await Vehicle.find({ owner });
-      res.status(200).json(vehicles);
+      redisClient.get("userVehicles", async (err, data) => {
+        if (err) {
+          console.log("redisClient.get ~ err:", err);
+        } else if (data != null) {
+          res.status(200).json(JSON.parse(data));
+        } else {
+          const vehicles = await Vehicle.find({ owner });
+          redisClient.setex(
+            "userVehicles",
+            expirationTime,
+            JSON.stringify(vehicles)
+          );
+        }
+      });
     } catch (error) {
       next(error);
     }
