3a9f5d76897c32ead7a1502942e82c229af71dc2
Med Ali Naaoui <chayga00@gmail.com>
2023-11-01 21:57:56 +0100

    redis

--- api/controllers/vehicleController.js
+++ api/controllers/vehicleController.js
@@ -1,10 +1,14 @@
 import Vehicle from "../model/vehicle.js";
+import sendOrStore from "../model/redisFn.js";
 
 const vehiclesListController = async (req, res, next) => {
   const vehicleId = req.params.id;
   console.log("vehicleId:", vehicleId);
   try {
-    const vehicle = await Vehicle.findById(vehicleId);
+    const vehicle = await sendOrStore(`vehicle?id=${vehicleId}`, async () => {
+      const vehicle = await Vehicle.findById(vehicleId);
+      return vehicle;
+    });
 
     console.log("vehicle:", vehicle);
 
