c9624bf96ce4fb58f8bc33d7433b1d59ad77bab1
Med Ali Naaoui <chayga00@gmail.com>
2023-11-04 15:19:15 +0100

    home landscape

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,54 @@
 import { useSelector } from "react-redux";
+import { main } from "../payload";
+import carPic from "../assets/3fab99ff58fd2951976ae3d367d6428b.png";
+import audi from "../assets/2428e95a0fdbce6c630703a99e6be1f0.png";
+import vols from "../assets/96d4d6eff7fe7a67efd77d646500075b.jpg";
+import bmw from "../assets/9f456ebcb2b0598ae566274cf7da0532.png";
+import merce from "../assets/00bcfe6faa96a02f7ac6bbae10842ea1.png";
 
 const Home = () => {
   const currUser = useSelector((state) => state.user.currUser);
-  // console.log(currUser);
-  return <div>homeme</div>;
+  const lang = useSelector((state) => state.user.language) || "arabic";
+
+  return (
+    <div className="px-40 py-6 pt-16 ">
+      <div
+        className={`${
+          lang === "arabic" ? "flex-row-reverse " : ""
+        } main flex gap-4 relative items-center  pb-20 pt-8`}
+      >
+        <div className="text-right flex flex-col gap-4">
+          <h1 className="text-2xl font-semibold">{main[lang].mainHead}</h1>
+          <p className="tracking-tight ">{main[lang].mainPara}</p>
+          <div className="flex flex-row-reverse gap-3">
+            <button className="btn btn-outline btn-warning">
+              {main[lang].addCar}
+            </button>
+            <button className="btn btn-outline btn-info">
+              {main[lang].carList}
+            </button>
+          </div>
+        </div>
+        <div className="z-10">
+          <img className="" src={carPic} alt="car pic" />
+        </div>
+        <div className="behind-the-car h-full w-60 rounded-md absolute left-10 top-0"></div>
+      </div>
+      <div className="brands flex justify-around px-40 items-center">
+        <div className="one">
+          <img src={audi} alt="audi" />
+        </div>
+        <div className="two">
+          <img src={vols} alt="volvo" />
+        </div>
+        <div className="three">
+          <img src={bmw} alt="bmw" />
+        </div>
+        <div className="four">
+          <img src={merce} alt="mercedes" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Home;
--- client/src/payload.js
+++ client/src/payload.js
@@ -21,3 +21,83 @@ export const navbarData = {
     register: "Register",
   },
 };
+
+export const main = {
+  arabic: {
+    mainHead: "تبحث عن إيجار ولكنك لا تعرف أين تبحث؟",
+    mainPara:
+      "هذه المنصة هي وجهتك الرئيسية للإيجار والبحث عن سيارات للإيجار. تم تصميمها لتوفير تجربة مريحة وموثوقة للأفراد الذين يتطلعون للعثور على سيارة للاستئجار أو لأولئك الذين يرغبون في تأجير سيارتهم. تعتبر هذه المنصة مكانًا يجمع بين المؤجرين والمستأجرين، مما يسهل العثور على السيارة المناسبة بسرعة وسهولة.",
+    addCar: "إضافة سيارة",
+    carList: "قائمة السيرات",
+  },
+  english: {
+    mainHead: "Looking To Rent But Don’t Know Where To Look?",
+    mainPara:
+      "This platform is your primary destination for renting cars and finding cars to rent. It has been designed to provide a convenient and reliable experience for individuals who are looking to find a rental car or for those who want to rent out their cars. This platform brings together both renters and rentees, making it easy to quickly and easily find the right car.",
+    addCar: "Include a Vehicle",
+    carList: "Vehicles List",
+  },
+};
+
+export const howToStart = {
+  arabic: {
+    add: {
+      stepOne: {
+        title: "أنشئ إعلان",
+        para: "شارك سيارتك مع المستأجرين عبر إنشاء إعلان مفصل. قدم معلومات حول سيارتك",
+      },
+      stepTwo: {
+        title: "إدارة الطلبات",
+        para: "قم بمراجعة طلبات الزبناء وقرر ما إذا كنت ستقبلها أو ترفضها ",
+      },
+      stepThree: {
+        title: " تواصل مع المستأجر",
+        para: "قم بالتنسيق مع المستأجر للإستلام والإعادة. بعد عملية تأجير ناجحة",
+      },
+    },
+    rent: {
+      stepOne: {
+        title: "تصفح السيارات",
+        para: "ابدأ رحلتك في استئجار السيارة من خلال تصفح مجموعة متنوعة من السيارات المتاحة",
+      },
+      stepTwo: {
+        title: "اختر سيارتك",
+        para: "بمجرد أن تجد السيارة المثالية لك، انقر عليها لعرض المزيد من التفاصيل، بما في ذلك الصور والتسعير ومعلومات المالك",
+      },
+      stepThree: {
+        title: "احجز وانطلق",
+        para: "قم بالتنسيق مع المالك للإستلام واستكمال عملية الحجز",
+      },
+    },
+  },
+  english: {
+    add: {
+      stepOne: {
+        title: "Create a Listing",
+        para: "Share your car with potential renters by creating a detailed listing. Provide information about your vehicle.",
+      },
+      stepTwo: {
+        title: "Manage Requests",
+        para: "Review booking requests from renters and decide whether to accept or decline them.",
+      },
+      stepThree: {
+        title: "Communicate with the Renter",
+        para: "Coordinate with the renter for pick-up and return. After a successful rental.",
+      },
+    },
+    rent: {
+      stepOne: {
+        title: "Browse Cars",
+        para: "Start your car rental journey by browsing through a diverse selection of available vehicles.",
+      },
+      stepTwo: {
+        title: "Choose Your Ride",
+        para: "Once you've found the perfect car, click on it to view more details, including photos, pricing, and owner information.",
+      },
+      stepThree: {
+        title: "Book and Drive",
+        para: "After selecting your ideal car, make a reservation, and complete the booking process. You're now ready to hit the road.",
+      },
+    },
+  },
+};
